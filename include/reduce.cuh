3c9882ca8c77362bdb843745058242ed9a11d591
yft1996 <44723308+yft1996@users.noreply.github.com>
2019-03-26 10:40:18 +0800

    修改头文件
    在循环内全展开的基础上使用共享内存

--- include/reduce.cuh
+++ include/reduce.cuh
@@ -37,8 +37,9 @@ __global__ void reduceCompleteUnroll(int *g_idata, int *g_odata,unsigned int n)
     unsigned int idx = blockIdx.x * blockDim.x * 8 + threadIdx.x;//全局索引（展开循环８次）
 
     // 将全局数据指针转换为此块的本地指针
-    int *idata = g_idata + blockIdx.x * blockDim.x * 8;
-
+    //int *idata = g_idata + blockIdx.x * blockDim.x * 8;
+    //使用共享内存
+    __shared__ int s[iBlockSize];
     // 读取８个线程块大小的数据量
     if (idx + 7 * blockDim.x < n)
     {
@@ -50,27 +51,27 @@ __global__ void reduceCompleteUnroll(int *g_idata, int *g_odata,unsigned int n)
         int b2 = g_idata[idx + 5 * blockDim.x];
         int b3 = g_idata[idx + 6 * blockDim.x];
         int b4 = g_idata[idx + 7 * blockDim.x];
-        g_idata[idx] = a1 + a2 + a3 + a4 + b1 + b2 + b3 + b4;
+        s[tid] = a1 + a2 + a3 + a4 + b1 + b2 + b3 + b4;
     }
     __syncthreads();
 
     // 完全展开for循环,线程块＝＝512时，从第二个判断语句开始执行
-    if (iBlockSize >= 1024 && tid < 512) idata[tid] += idata[tid + 512];
+    if (iBlockSize >= 1024 && tid < 512) s[tid] += s[tid + 512];
     __syncthreads();
 
-    if (iBlockSize >= 512 && tid < 256)  idata[tid] += idata[tid + 256];
+    if (iBlockSize >= 512 && tid < 256)  s[tid] += s[tid + 256];
     __syncthreads();
 
-    if (iBlockSize >= 256 && tid < 128)  idata[tid] += idata[tid + 128];
+    if (iBlockSize >= 256 && tid < 128)  s[tid] += s[tid + 128];
     __syncthreads();
 
-    if (iBlockSize >= 128 && tid < 64)   idata[tid] += idata[tid + 64];
+    if (iBlockSize >= 128 && tid < 64)   s[tid] += s[tid + 64];
     __syncthreads();
 
     // 展开线程束
     if (tid < 32)
     {
-        volatile int *vsmem = idata;//volatile 关键字起屏障作用
+        volatile int *vsmem = s;//volatile 关键字起屏障作用
         vsmem[tid] += vsmem[tid + 32];
         vsmem[tid] += vsmem[tid + 16];
         vsmem[tid] += vsmem[tid +  8];
@@ -79,7 +80,7 @@ __global__ void reduceCompleteUnroll(int *g_idata, int *g_odata,unsigned int n)
         vsmem[tid] += vsmem[tid +  1];
     }
     // 结果写回全局内存
-    if (tid == 0) g_odata[blockIdx.x] = idata[0];
+    if (tid == 0) g_odata[blockIdx.x] = s[0];
 }
 
 template <unsigned int iBlockSize>
@@ -89,8 +90,9 @@ __global__ void reduce_max(int *g_idata,int *g_odata,unsigned int n)
     unsigned int idx = blockIdx.x * blockDim.x * 2 + threadIdx.x;//全局索引（展开循环８次）
 
     // 将全局数据指针转换为此块的本地指针
-    int *idata = g_idata + blockIdx.x * blockDim.x * 8;
-
+    //int *idata = g_idata + blockIdx.x * blockDim.x * 8;
+    //使用共享内存
+    __shared__ int idata[iBlockSize];
     // 读取８个线程块大小的数据量
     if (idx + 7 * blockDim.x < n)
     {
@@ -102,7 +104,7 @@ __global__ void reduce_max(int *g_idata,int *g_odata,unsigned int n)
         int b2 = g_idata[idx + 5 * blockDim.x];
         int b3 = g_idata[idx + 6 * blockDim.x];
         int b4 = g_idata[idx + 7 * blockDim.x];
-        g_idata[idx] = max(a1,max(a2,max(a3,max(a4,max(b1,max(b2,max(b3,b4)))))));
+        idata[tid] = max(a1,max(a2,max(a3,max(a4,max(b1,max(b2,max(b3,b4)))))));
     }
     __syncthreads();
 
@@ -141,8 +143,9 @@ __global__ void reduce_min(int *g_idata,int *g_odata,unsigned int n)
     unsigned int idx = blockIdx.x * blockDim.x * 2 + threadIdx.x;//全局索引（展开循环８次）
 
     // 将全局数据指针转换为此块的本地指针
-    int *idata = g_idata + blockIdx.x * blockDim.x * 8;
-
+    //int *idata = g_idata + blockIdx.x * blockDim.x * 8;
+    //使用共享内存
+    __shared__ int idata[iBlockSize];
     // 读取８个线程块大小的数据量
     if (idx + 7 * blockDim.x < n)
     {
@@ -154,7 +157,7 @@ __global__ void reduce_min(int *g_idata,int *g_odata,unsigned int n)
         int b2 = g_idata[idx + 5 * blockDim.x];
         int b3 = g_idata[idx + 6 * blockDim.x];
         int b4 = g_idata[idx + 7 * blockDim.x];
-        g_idata[idx] = min(a1,min(a2,min(a3,min(a4,min(b1,min(b2,min(b3,b4)))))));
+        idata[tid] = min(a1,min(a2,min(a3,min(a4,min(b1,min(b2,min(b3,b4)))))));
     }
     __syncthreads();
 
